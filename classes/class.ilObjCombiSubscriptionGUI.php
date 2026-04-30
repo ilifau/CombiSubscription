a945d34455e0d92f13495c86828747481a00491a
chrfau <chr.fuchs@fau.de>
2026-04-30 15:50:17 +0200

    fix alert icon

--- classes/class.ilObjCombiSubscriptionGUI.php
+++ classes/class.ilObjCombiSubscriptionGUI.php
@@ -417,7 +417,8 @@ class ilObjCombiSubscriptionGUI extends ilObjectPluginGUI
 				return ilUtil::getImagePath('scorm/failed.svg');
 			case ilObjCombiSubscription::SATISFIED_OVER:
 			case ilObjCombiSubscription::SATISFIED_CONFLICT:
-				return ilUtil::getImagePath('icon_alert.svg');
+                return $this->plugin->getRelativeDirectory() . "/templates/images/icon_alert_xcos.svg";
+				// return ilUtil::getImagePath('icon_alert.svg');
 			case ilObjCombiSubscription::SATISFIED_EMPTY:
             case ilObjCombiSubscription::SATISFIED_UNKNOWN:
 			default:
